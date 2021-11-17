965591883ad9ddc64a9901d9192e079abe6ced4f
MarceloMacedo02 <marcelo_macedo01@hotmail.com>
2021-11-16 21:47:11 -0300

    atualizado para modo de producao

--- src/main/java/br/com/zonesoftware/apicombustivel/CombustiveisServerApplication.java
+++ src/main/java/br/com/zonesoftware/apicombustivel/CombustiveisServerApplication.java
@@ -28,19 +28,19 @@ public class CombustiveisServerApplication {
 
     private void initUser(UsuarioService usuarioService, PasswordEncoder passwordEncoder){
 
-        Usuario usuario=null;
-        usuario =new Usuario();// Usuario.builder()
-        usuario.setEmail("admin@gmail.com");
-        usuario.setSenha(passwordEncoder.encode("123456"));
-        usuario.setPerfil(PerfilAcesso.SUPER_ADMINISTRADOR);
-              // .build();
-
-        usuario.setNome("Fulano de Tal dos Anzóis");
-        usuario.setDocumento("20073940326");
-
-        if(usuarioService.findByEmail(usuario.getEmail()) == null){
-            usuarioService.save(usuario);
-        }
+//        Usuario usuario=null;
+//        usuario =new Usuario();// Usuario.builder()
+//        usuario.setEmail("admin@gmail.com");
+//        usuario.setSenha(passwordEncoder.encode("123456"));
+//        usuario.setPerfil(PerfilAcesso.SUPER_ADMINISTRADOR);
+//              // .build();
+//
+//        usuario.setNome("Fulano de Tal dos Anzóis");
+//        usuario.setDocumento("20073940326");
+//
+//        if(usuarioService.findByEmail(usuario.getEmail()) == null){
+//            usuarioService.save(usuario);
+//        }
 //        for (int i = 0; i < 30;i++) {
 //        	usuario =new Usuario();// Usuario.builder()
 //            usuario.setEmail("admin@gmail.com" + ""+i);
